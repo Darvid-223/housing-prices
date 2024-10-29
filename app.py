f70d95cff4fe1c89e4e74ced4680c5169d571749
David Arvidsson <david.arvidsson@hotmail.com>
2024-10-29 20:45:16 +0000

    Hypothesis added to app.py

--- app.py
+++ app.py
@@ -3,11 +3,12 @@ This file and its contents were inspired by and adapted from the Churnometer Wal
 '''
 
 import streamlit as st
-from app_pages.multipage import MultiPage
+from app_pages.multi_page import MultiPage
 
 
 from app_pages.page_summary import page_summary_body
-from app_pages.predict_sales_price import predict_sales_price
+from app_pages.page_hypothesis import page_hypothesis
+# from app_pages.predict_sales_price import predict_sales_price
 
 
 
@@ -16,7 +17,8 @@ app = MultiPage(app_name= "Housing Prices") # Create an instance of the app
 
 # Add your app pages here using .add_page()
 app.add_page("Project Summary", page_summary_body)
-app.add_page("House price prediction", predict_sales_price)
+app.add_page("Hypothesis", page_hypothesis)
+#app.add_page("House price prediction", predict_sales_price)
 
 
 app.run()  # Run the app
